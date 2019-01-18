81427d59026740a5b065dceae883808e2ec7ebce
arthur ming <mingguobin@live.com>
2019-01-18 09:56:39 +0800

    fix: 變量可以為undefined

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import { Observable } from 'rxjs';
 
 export class RxHttpService {
 
-    private httpService: HttpService;
+    private httpService: HttpService | undefined;
 
     // http<T>(method: string, url: string, data: any, params: any, headers: Header<string>): Observable<T> {
     //     this.httpService.http(method, url, data, params, headers)
