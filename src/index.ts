08e6ac80eb90a38f151506df68ff6c8516faedf3
arthur ming <mingguobin@live.com>
2019-01-18 17:44:35 +0800

    feat: 完成一个基本的基于rxjs的http请求

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,17 @@ import { Observable } from 'rxjs';
 
 export class RxHttpService {
 
-    private httpService: HttpService | undefined;
+    private httpService: HttpService;
 
-    // http<T>(method: string, url: string, data: any, params: any, headers: Header<string>): Observable<T> {
-    //     this.httpService.http(method, url, data, params, headers)
-    //     return  null;
-    // }
+    http<T>(method: string, url: string, data: any, params: any, headers: Header<string>): Observable<T> | null {
+        this.httpService.http(method, url, data, params, headers)
+        return Observable.create( (observer: { next: (value: T) => void; error: (value: any) => void; }) => {
+            this.httpService.http<T>(method, url, data, params, headers)
+            .then(res => observer.next(res)).catch(error => observer.error(error));
+        })
+    }
+    constructor() {
+        this.httpService = new HttpService();
+    }
+
 }
