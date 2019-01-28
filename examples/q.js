8936d5fa32331bf52025029a4061f558dda1d001
明国宾 <mingguobin@live.com>
2019-01-28 21:19:18 +0800

    doc: 完善q.js的demo

--- examples/q.js
+++ examples/q.js
@@ -1,4 +1,7 @@
 const http = require('./http').HttpService;
 
-const q = new http().http('get', 'https://www.tianqiapi.com/api/?version=v1&city=上海');
+const q = new http().http('get', 'https://www.tianqiapi.com/api',{}, {
+    version: 'v1',
+    city: '上海',
+});
 q.then(console.log).catch(console.log);
