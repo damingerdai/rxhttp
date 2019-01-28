1dc329f162820fbe8f0e047f391804eb3d0db962
明国宾 <mingguobin@live.com>
2019-01-28 21:26:41 +0800

    feat: 完成rxhttp开发

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,7 @@ export class RxHttpService {
 
     private httpService: HttpService;
 
-    http<T>(method: string, url: string, data: any, params: any, headers: Header<string>): Observable<T> | null {
-        this.httpService.http(method, url, data, params, headers)
+    http<T>(method: string, url: string, data: any, params: any, headers: Header<string>): Observable<T> {
         return Observable.create( (observer: { next: (value: T) => void; error: (value: any) => void; }) => {
             this.httpService.http<T>(method, url, data, params, headers)
             .then(res => observer.next(res)).catch(error => observer.error(error));
